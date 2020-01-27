4856b7dc2906e652e8477d95b9ab149bf578c652
Brian Yeung <brianyeung@live.com>
2020-01-27 15:57:09 -0500

    update m1.h

--- main/src/libstreetmap/src/m1.h
+++ main/src/libstreetmap/src/m1.h
@@ -22,10 +22,29 @@
 using namespace std;
 
 extern vector<Street> streets;
-//load map
+//load and close map
 bool load_map(string map_name);
-
-//close map
+void cleanup_data_structures();
 void close_map();
 
+//load map info
+void load_streets();
+void load_intersections();
+void load_poi();
 
+bool are_directly_connected(unsigned id1, unsigned id2);
+
+std::vector<unsigned> find_street_ids_from_name(std::string street_name);
+std::vector<unsigned> find_intersection_street_segments(unsigned intersection_id);
+std::vector<std::string> find_intersection_street_names(unsigned intersection_id);
+std::vector<unsigned> find_adjacent_intersections(unsigned intersection_id);
+
+std::vector<unsigned> find_street_street_segments(unsigned street_id);
+std::vector<unsigned> find_all_street_intersections(unsigned street_id);
+std::vector<unsigned> find_intersection_ids_from_street_names(std::string street2, std::string street2);
+
+double find_distance_between_two_points(LatLon point1, LatLon point2);
+double find_street_segment_length(unsigned segment_id)
+double find_street_length(unsigned street_id);
+
+double find_street_segment_travel_time(unsigned segment_id);
